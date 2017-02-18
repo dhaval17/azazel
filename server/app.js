659cbaba9152ca3a7a6c1bdc3e53636894a29b31
Dhaval Chauhan <dhaval@dr4cun0.com>
2017-02-18 18:24:08 -0500

    limits

--- server/app.js
+++ server/app.js
@@ -1,4 +1,16 @@
 var express = require('express');
+var mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/azazel');
+
+
+var log = mongoose.model('log', 
+	{ 
+		source: String,
+		headers: String,
+		location: String
+	}
+);
 
 var app = express();
 var cors = require('cors')
@@ -10,17 +22,30 @@ var corsOptions = {
 
 
 var bodyParser = require('body-parser');
-app.use(bodyParser.json({limit: '50mb'}));
-app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
+app.use(bodyParser.json({limit: '10mb'}));
+app.use(bodyParser.urlencoded({limit: '10mb', extended: true}));
 
-app.get('/logger/', function(req, res) {
+app.get('/logger/', cors(corsOptions), function(req, res) {
 	res.send('hello');
 });
 
-app.post('/logger', cors(corsOptions), function (req, res) {
-  res.send('Hello World');
-  console.log(req.body.source + " " + req.body.headers);
-  //console.log(req);
+app.post('/logger/', cors(corsOptions), function (req, res) {
+	
+  	res.send('Hello World');
+  	//console.log(req.body.source + " " + req.body.headers);
+  	//console.log(req);
+  	//console.log(req.body.source.length + "\n");
+  	
+  	var logger = new log({ source: req.body.source, headers: req.body.headers, location: req.body.location });
+  	
+	logger.save(function (err) {
+	  if (err) {
+	    //console.log(err);
+	  } else {
+	    //console.log('meow');
+	  }
+	});
+	
 })
  
 app.listen(31337);
